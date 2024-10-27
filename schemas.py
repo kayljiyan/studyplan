b301a8c0b4c1e54dffd509802464d8d0659e9715
kyleg <kylegianlatina@gmail.com>
2024-10-28 00:33:03 +0800

    changed update logic

--- schemas.py
+++ schemas.py
@@ -43,7 +43,6 @@ class TaskAddToDB(BaseModel):
     task_priority: str
     task_category: str
     task_deadline: datetime
-    is_done: bool
     user_uuid: UUID4
 
 class TaskUpdateToDB(TaskAddToDB):
