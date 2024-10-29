9436341417cf5ecd51b5382365ab52ba2248790a
kyleg <kylegianlatina@gmail.com>
2024-10-29 15:13:19 +0800

    fixing middleware

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://studyplan-frontend.vercel.app"],
+    allow_origins=["http://localhost:5173","https://studyplan-one.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
