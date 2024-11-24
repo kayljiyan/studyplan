4c1cb52ae6559e6972668499b4b13ec06eed3218
kayljiyan <kylegianlatina@gmail.com>
2024-11-24 21:30:06 +0800

    fixed registration

--- models.py
+++ models.py
@@ -1,8 +1,21 @@
-from sqlalchemy import Column, ForeignKey, UUID, String, Float, DateTime, Boolean, Integer
 from typing import List
-from sqlalchemy.orm import relationship, mapped_column, Mapped
+
+from sqlalchemy import (
+    UUID,
+    Boolean,
+    Column,
+    DateTime,
+    Float,
+    ForeignKey,
+    Integer,
+    String,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+import schemas as schemas
+import security as security
 from dbconf import Base
-import schemas as schemas, security as security
+
 
 class Sprite(Base):
     __tablename__ = "sprites"
@@ -11,7 +24,10 @@ class Sprite(Base):
     sprite_source = Column(String, nullable=False, unique=True)
     sprite_summon_chance = Column(Float, nullable=False)
 
-    sprite_instances: Mapped[List["SpriteInstance"]] = relationship(back_populates="sprite")
+    sprite_instances: Mapped[List["SpriteInstance"]] = relationship(
+        back_populates="sprite"
+    )
+
 
 class Forum(Base):
     __tablename__ = "forums"
@@ -24,7 +40,10 @@ class Forum(Base):
     created_at = Column(DateTime, nullable=True, default=security.get_locale_datetime())
 
     forum_members: Mapped[List["ForumMember"]] = relationship(back_populates="forum")
-    forum_comments: Mapped[List["ForumComment"]] = relationship("ForumComment", back_populates="forum")
+    forum_comments: Mapped[List["ForumComment"]] = relationship(
+        "ForumComment", back_populates="forum"
+    )
+
 
 class User(Base):
     __tablename__ = "users"
@@ -36,15 +55,18 @@ class User(Base):
     user_password = Column(String, nullable=False)
     is_premium = Column(Boolean, nullable=False, default=False)
     is_confirmed = Column(Boolean, nullable=False, default=False)
-    user_points = Column(Integer, nullable=False)
+    user_points = Column(Integer, nullable=False, default=0)
     push_notif = Column(Boolean, nullable=False, default=False)
     user_avatar = Column(String, nullable=False)
 
-    sprite_instances: Mapped[List["SpriteInstance"]] = relationship(back_populates="user")
+    sprite_instances: Mapped[List["SpriteInstance"]] = relationship(
+        back_populates="user"
+    )
     tasks: Mapped[List["Task"]] = relationship(back_populates="user")
     forum_comments: Mapped[List["ForumComment"]] = relationship(back_populates="user")
     forum_members: Mapped[List["ForumMember"]] = relationship(back_populates="user")
     # user_logs: Mapped[List["UserLog"]] = relationship(back_populates="user")
+
 
 class Task(Base):
     __tablename__ = "tasks"
@@ -60,11 +82,16 @@ class Task(Base):
     user: Mapped["User"] = relationship(back_populates="tasks")
     # task_logs: Mapped[List["TaskLog"]] = relationship(back_populates="task")
 
+
 class SpriteInstance(Base):
     __tablename__ = "sprite_instances"
 
-    sprite_instance_uuid = mapped_column(UUID, primary_key=True, default=security.generate_uuid)
-    acquisition_date = Column(DateTime, nullable=False, default=security.get_locale_datetime)
+    sprite_instance_uuid = mapped_column(
+        UUID, primary_key=True, default=security.generate_uuid
+    )
+    acquisition_date = Column(
+        DateTime, nullable=False, default=security.get_locale_datetime
+    )
     sprite_uuid = mapped_column(UUID, ForeignKey("sprites.sprite_uuid"), nullable=False)
     user_uuid = mapped_column(UUID, ForeignKey("users.user_uuid"), nullable=False)
 
@@ -72,10 +99,13 @@ class SpriteInstance(Base):
     sprite: Mapped["Sprite"] = relationship(back_populates="sprite_instances")
     # sprite_instance_logs: Mapped[List["SpriteInstanceLog"]] = relationship(back_populates="sprite_instance")
 
+
 class ForumMember(Base):
     __tablename__ = "forum_members"
 
-    forum_member_uuid = mapped_column(UUID, primary_key=True, default=security.generate_uuid)
+    forum_member_uuid = mapped_column(
+        UUID, primary_key=True, default=security.generate_uuid
+    )
     user_name = Column(String, nullable=False)
     is_owner = Column(Boolean, nullable=False)
     created_at = Column(DateTime, nullable=True, default=security.get_locale_datetime())
@@ -85,17 +115,23 @@ class ForumMember(Base):
     forum: Mapped["Forum"] = relationship(back_populates="forum_members")
     user: Mapped["User"] = relationship(back_populates="forum_members")
 
+
 class ForumComment(Base):
     __tablename__ = "forum_comments"
 
-    forum_comment_uuid = mapped_column(UUID, primary_key=True, default=security.generate_uuid)
+    forum_comment_uuid = mapped_column(
+        UUID, primary_key=True, default=security.generate_uuid
+    )
     forum_comment = Column(String, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=security.get_locale_datetime())
+    created_at = Column(
+        DateTime, nullable=False, default=security.get_locale_datetime()
+    )
     forum_uuid = mapped_column(UUID, ForeignKey("forums.forum_uuid"), nullable=False)
     user_uuid = mapped_column(UUID, ForeignKey("users.user_uuid"), nullable=False)
 
     forum: Mapped["Forum"] = relationship(back_populates="forum_comments")
     user: Mapped["User"] = relationship(back_populates="forum_comments")
+
 
 # class TaskLog(Base):
 #     __tablename__ = "task_logs"
