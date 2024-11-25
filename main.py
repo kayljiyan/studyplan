d73607f77b6ae059d64b0b12eb1eaa24b82e1367
kayljiyan <kylegianlatina@gmail.com>
2024-11-25 11:57:37 +0800

    fix redirect link

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ async def confirm_email(
     try:
         dbops.confirm_email(db, user_email)
         response.status_code = status.HTTP_308_PERMANENT_REDIRECT
-        return RedirectResponse("https://studyplan-frontend.vercel.app/")
+        return RedirectResponse("https://studyplan-one.vercel.app/")
     except Exception as e:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"detail": str(e)}
